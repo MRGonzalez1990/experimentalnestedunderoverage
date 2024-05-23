c8a0814cd71a88b2b08d35323393a23ca21bcef9
MRGonzalez1990 <maurogonzalezplus@gmail.com>
2024-05-23 11:06:55 -0700

    Update myexperimentalnestedunderoverage.py

--- myexperimentalnestedunderoverage.py
+++ myexperimentalnestedunderoverage.py
@@ -1,7 +1,12 @@
 #Author Mauro Ruben Gonzalez
+#'experimental nested under over age' application
+#Python3
+
 x=input('What is your age?')
 if int(x) >= 18:
     print('Adult')
 elif int(x)<18:
         print('Underage')
+
+#Copyright 1990 Mauro Ruben Gonzalez
         
